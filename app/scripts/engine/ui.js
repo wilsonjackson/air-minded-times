94bd3161f08a36a100f304a67d3fe5f401c01497
Majid Burney <moocow@euphoricsoup.com>
2014-06-24 18:12:27 -0700

    Effects

--- app/scripts/engine/ui.js
+++ app/scripts/engine/ui.js
@@ -1,6 +1,6 @@
 /* global Game */
 
-(function (Game) {
+(function (Game, document) {
 	'use strict';
 
 	function CoverScreen() {}
@@ -94,8 +94,131 @@
 		};
 	})();
 
+	//noinspection JSUnusedGlobalSymbols
+	var Effects = {
+		snapshotCanvas: function () {
+			var mainCanvas = Game.getViewport().canvas;
+			var snapshot = document.createElement('canvas');
+			snapshot.setAttribute('width', mainCanvas.getAttribute('width'));
+			snapshot.setAttribute('height', mainCanvas.getAttribute('height'));
+			snapshot.style.display = 'none';
+			snapshot.style.position = 'absolute';
+			snapshot.style.left = '0';
+			snapshot.style.top = '0';
+			mainCanvas.parentElement.insertBefore(snapshot, mainCanvas.nextSibling);
+			snapshot.getContext('2d').drawImage(mainCanvas, 0, 0);
+			return snapshot;
+		},
+
+		fadeOut: function (ticks, callback) {
+			ticks = ticks || 30;
+			var tick = 0;
+			var snapshot = this.snapshotCanvas();
+			var screen = new CoverScreen();
+			screen.update = function () {
+				if (tick === ticks) {
+					snapshot.parentElement.removeChild(snapshot);
+					Ui.deactivateScreen();
+					if (callback) {
+						callback();
+					}
+					return;
+				}
+				++tick;
+			};
+			screen.render = function (graphics) {
+				var context = graphics.viewport.context;
+				context.drawImage(snapshot, 0, 0);
+				context.globalAlpha = (1 / ticks) * tick;
+				context.fillStyle = '#000';
+				context.fillRect(0, 0, graphics.viewport.width, graphics.viewport.height);
+				context.globalAlpha = 1;
+			};
+			Ui.activateScreen(screen);
+		},
+
+		mosaic: function (steps, stepTicks, callback) {
+			steps = steps || 8;
+			stepTicks = stepTicks || 6;
+
+			var step = 1;
+			var lastStep = 0;
+			var tick = 0;
+			var snapshot = this.snapshotCanvas();
+			var snapshotContext = snapshot.getContext('2d');
+			var pixels = snapshotContext.getImageData(0, 0, snapshot.width, snapshot.height).data;
+			var stepImageData = snapshotContext.createImageData(snapshot.width, snapshot.height);
+			var stepPixels = stepImageData.data;
+			var screen = new CoverScreen();
+			screen.update = function () {
+				if (step === steps && tick === stepTicks) {
+					snapshot.parentElement.removeChild(snapshot);
+					Ui.deactivateScreen();
+					if (callback) {
+						callback();
+					}
+					return;
+				}
+				if (tick === stepTicks) {
+					++step;
+					tick = 0;
+				}
+				++tick;
+			};
+			screen.render = function (graphics) {
+				var pixelsPerTile = 4 * step;
+				var xTiles = Math.floor(snapshot.width / pixelsPerTile);
+				var yTiles = Math.floor(snapshot.height / pixelsPerTile);
+
+				var reds;
+				var greens;
+				var blues;
+				var i, j;
+				if (lastStep !== step) {
+					for (var x = 0; x < xTiles; x++) {
+						var tileX = x * pixelsPerTile * 4;
+						for (var y = 0; y < yTiles; y++) {
+							var tileY = y * pixelsPerTile * 4;
+							var cornerIdx = tileY * snapshot.width + tileX;
+							reds = 0;
+							greens = 0;
+							blues = 0;
+							var n = 0;
+							var pixelIdx;
+							for (i = 0; i < pixelsPerTile; i++) {
+								for (j = 0; j < pixelsPerTile; j++) {
+									pixelIdx = (cornerIdx + j * snapshot.width * 4 + i * 4);
+									reds += pixels[pixelIdx];
+									greens += pixels[pixelIdx + 1];
+									blues += pixels[pixelIdx + 2];
+									n++;
+								}
+							}
+							var red = Math.round(reds / n);
+							var green = Math.round(greens / n);
+							var blue = Math.round(blues / n);
+							for (i = 0; i < pixelsPerTile; i++) {
+								for (j = 0; j < pixelsPerTile; j++) {
+									pixelIdx = (cornerIdx + j * snapshot.width * 4 + i * 4);
+									stepPixels[pixelIdx] = red;
+									stepPixels[pixelIdx+1] = green;
+									stepPixels[pixelIdx+2] = blue;
+									stepPixels[pixelIdx+3] = 255;
+								}
+							}
+						}
+					}
+					snapshotContext.putImageData(stepImageData, 0, 0);
+				}
+				graphics.viewport.context.drawImage(snapshot, 0, 0);
+			};
+			Ui.activateScreen(screen);
+		}
+	};
+
 	Game.ui = {
 		Ui: Ui,
-		CoverScreen: CoverScreen
+		CoverScreen: CoverScreen,
+		Effects: Effects
 	};
-})(Game);
+})(Game, document);
